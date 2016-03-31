8a34df22682fe78c288dc0ea51c6fa46a7ee50c0
Tobias Bieniek <tobias.bieniek@gmail.com>
2016-03-31 20:39:02 +0200

    test: Add .to.contain assertions

--- test/test.js
+++ test/test.js
@@ -42,4 +42,60 @@ describe('expect(file(...))', function() {
       });
     });
   });
+
+  describe('.to.contain', function() {
+    it('passes for file containing search string', function() {
+      expect(file('test/fixtures/foo.txt')).to.contain('small fixture file');
+    });
+
+    it('fails for file missing search string', function() {
+      expect(function() {
+        expect(file('test/fixtures/foo.txt')).to.contain('large solid object');
+      }).to.throw(function(err) {
+        expect(err.toString()).to.equal('Error: expected \"test/fixtures/foo.txt\" to contain \"large solid object\"');
+        expect(err.showDiff).to.be.not.ok;
+        expect(err.actual).to.contain('small fixture file');
+        expect(err.expected).to.equal('large solid object');
+      });
+    });
+
+    it('fails for missing files', function() {
+      expect(function() {
+        expect(file('index.coffee')).to.contain('large solid object');
+      }).to.throw(function(err) {
+        expect(err.toString()).to.equal('Error: expected \"index.coffee\" to exist');
+        expect(err.showDiff).to.be.not.ok;
+        expect(err.actual).to.not.exist;
+        expect(err.expected).to.not.exist;
+      });
+    });
+  });
+
+  describe('.to.not.contain', function() {
+    it('passes for file missing search string', function() {
+      expect(file('test/fixtures/foo.txt')).to.not.contain('large solid object');
+    });
+
+    it('fails for file containing search string', function() {
+      expect(function() {
+        expect(file('test/fixtures/foo.txt')).to.not.contain('small fixture file');
+      }).to.throw(function(err) {
+        expect(err.toString()).to.equal('Error: expected \"test/fixtures/foo.txt\" to not contain \"small fixture file\"');
+        expect(err.showDiff).to.be.not.ok;
+        expect(err.actual).to.not.exist;
+        expect(err.expected).to.not.exist;
+      });
+    });
+
+    it('fails for missing files', function() {
+      expect(function() {
+        expect(file('index.coffee')).to.not.contain('small fixture file');
+      }).to.throw(function(err) {
+        expect(err.toString()).to.equal('Error: expected \"index.coffee\" to exist');
+        expect(err.showDiff).to.be.not.ok;
+        expect(err.actual).to.not.exist;
+        expect(err.expected).to.not.exist;
+      });
+    });
+  });
 });
