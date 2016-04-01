3b3e3d914f41a09516afff72bba8e856f7c2182a
Tobias Bieniek <tobias.bieniek@gmail.com>
2016-04-01 17:04:45 +0200

    file-helper: Define dynamic "exists", "absolutePath" and "content" properties

--- src/file-helper.js
+++ src/file-helper.js
@@ -7,50 +7,57 @@ function FileHelper(path) {
   this._content = null;
 }
 
-FileHelper.prototype._absPath = function() {
-  return path.resolve(process.cwd(), this.path);
-};
+Object.defineProperty(FileHelper.prototype, 'absolutePath', {
+  get: function() {
+    return path.resolve(process.cwd(), this.path);
+  }
+});
 
-FileHelper.prototype.exists = function() {
-  if (this._exists === null) {
-    var path = this._absPath();
+Object.defineProperty(FileHelper.prototype, 'exists', {
+  get: function() {
+    if (this._exists === null) {
+      var path = this.absolutePath;
 
-    if (fs.existsSync) {
-      this._exists = fs.existsSync(path);
-    } else {
-      try {
-        fs.accessSync(path, fs.F_OK);
-        this._exists = true;
-      } catch (e) {
-        this._exists = false
+      if (fs.existsSync) {
+        this._exists = fs.existsSync(path);
+      } else {
+        try {
+          fs.accessSync(path, fs.F_OK);
+          this._exists = true;
+        } catch (e) {
+          this._exists = false
+        }
       }
     }
-  }
 
-  return this._exists;
-};
+    return this._exists;
+  }
+});
+
+Object.defineProperty(FileHelper.prototype, 'content', {
+  get: function() {
+    if (this._content === null) {
+      this._content = fs.readFileSync(this.absolutePath, {encoding: 'utf-8'});
+    }
+
+    return this._content;
+  }
+});
 
 FileHelper.prototype.assertExists = function() {
-  if (!this.exists()) {
+  if (!this.exists) {
     throw new Error('expected "' + this.path + '" to exist');
   }
 };
 
 FileHelper.prototype.assertDoesNotExist = function() {
-  if (this.exists()) {
+  if (this.exists) {
     throw new Error('expected "' + this.path + '" to not exist');
   }
 };
 
-FileHelper.prototype._loadContent = function() {
-  if (this._content === null) {
-    this._content = fs.readFileSync(this._absPath(), {encoding: 'utf-8'});
-  }
-};
-
 FileHelper.prototype.equals = function(str) {
-  this._loadContent();
-  return this._content === str;
+  return this.content === str;
 };
 
 FileHelper.prototype.assertEquals = function(value) {
@@ -59,13 +66,12 @@ FileHelper.prototype.assertEquals = function(value) {
   this.assertExists();
   if (valueIsFile) {
     value.assertExists();
-    value._loadContent();
   }
 
-  var str = valueIsFile ? value._content : value;
+  var str = valueIsFile ? value.content : value;
   if (!this.equals(str)) {
     var error = new Error('expected "' + this.path + '" to equal "' + (valueIsFile ? value.path : value) + '"');
-    error.actual = this._content;
+    error.actual = this.content;
     error.expected = str;
     throw error;
   }
@@ -77,25 +83,23 @@ FileHelper.prototype.assertDoesNotEqual = function(value) {
   this.assertExists();
   if (valueIsFile) {
     value.assertExists();
-    value._loadContent();
   }
 
-  var str = valueIsFile ? value._content : value;
+  var str = valueIsFile ? value.content : value;
   if (this.equals(str)) {
     throw new Error('expected "' + this.path + '" to not equal "' + (valueIsFile ? value.path : value) + '"');
   }
 };
 
 FileHelper.prototype.contains = function(str) {
-  this._loadContent();
-  return this._content.indexOf(str) !== -1;
+  return this.content.indexOf(str) !== -1;
 };
 
 FileHelper.prototype.assertContains = function(str) {
   this.assertExists();
   if (!this.contains(str)) {
     var error = new Error('expected "' + this.path + '" to contain "' + str + '"');
-    error.actual = this._content;
+    error.actual = this.content;
     error.expected = str;
     throw error;
   }
@@ -109,15 +113,14 @@ FileHelper.prototype.assertDoesNotContain = function(str) {
 };
 
 FileHelper.prototype.matches = function(regex) {
-  this._loadContent();
-  return regex.test(this._content);
+  return regex.test(this.content);
 };
 
 FileHelper.prototype.assertMatches = function(regex) {
   this.assertExists();
   if (!this.matches(regex)) {
     var error = new Error('expected "' + this.path + '" to match ' + regex);
-    error.actual = this._content;
+    error.actual = this.content;
     error.expected = regex;
     throw error;
   }
